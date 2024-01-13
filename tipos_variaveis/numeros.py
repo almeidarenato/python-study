5c3dd5234d211f11906e2c1e90e933e8709801b5
Renato Almeida <renatoam@ymail.com>
2024-01-13 00:41:30 -0300

    inclusao de numeros e conversoes

---
+++ tipos_variaveis/numeros.py
@@ -0,0 +1,44 @@
+# numeros do tipo inteiros
+numero_inteiro = 3
+print("Inteiro= ",numero_inteiro)
+
+# real com ponto flutuante
+numero_real = 3.14
+print("Real= ",numero_real)
+
+#type() - informa qual o tipo da variável
+print("O tipo é ", type(numero_real)) #<class 'float'>
+print("O tipo é ", type(numero_inteiro)) #<class 'int'>
+
+# Operações aritméticas
+
+#soma
+soma = 1 + 1
+print("Resultado",soma)
+
+#subtração
+subtracao = 1 - 1
+print("Resultado",subtracao)
+
+#multiplicaçao
+multiplicacao = 2 * 2 
+print("Resultado",multiplicacao)
+
+#divisao
+divisao = 5/2
+print("Resultado",divisao)
+print("Tipo", int(divisao))
+
+# Conversao
+
+#int() = converte float para inteiro 
+#float() = converte para float
+#exemplo 
+
+print("conversao de float para int",int(divisao))
+
+# Modulo
+
+modulo = 5 % 2
+
+print( ]Modulo,modulo )
