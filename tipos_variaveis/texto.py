9700ad2a27b5d5ba8c410da3a6412b530420285c
Renato Almeida <renatoam@ymail.com>
2024-01-13 20:59:11 -0300

    declaração de textos

---
+++ tipos_variaveis/texto.py
@@ -0,0 +1,12 @@
+# Declaração de textos . Recomendaçao do padrão é utilizar " " duplas 
+nome_completo = "Renato Almeida"
+
+# Declaração de texto que permite pular linhas
+nome_completo_aspas = """Permite pular 
+linhas """
+
+nome_completo_quebra = " Renato \
+    Almeida"
+
+nome = "Renato"
+sobrenome = "Almeida"
