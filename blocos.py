d7791963fdf511dfaf66536ee8ac5d86b568e998
Renato Almeida <renatoam@ymail.com>
2024-01-13 00:00:16 -0300

    adicionando estudo de blocos

---
+++ blocos.py
@@ -0,0 +1,20 @@
+# esse é meu comentário
+""" comentário de multipla linha """
+
+#concatenando nome
+outronome = "Renato" + "Almeida"
+
+#concatenando nome com valor em outra linha
+nome = "Renato" + \
+"Almeida"
+
+print(nome)
+print(outronome)
+
+#condicional e blocos
+
+idade = 26
+if idade < 18:
+    print("menor de idade")
+else:
+    print("maior de idade")
