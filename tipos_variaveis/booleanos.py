2aa6cdb345f2e8db837832193bd0c0b38d274212
Renato Almeida <renatoam@ymail.com>
2024-01-14 17:05:14 -0300

    adicionando tipo de dados booleanos

---
+++ tipos_variaveis/booleanos.py
@@ -0,0 +1,24 @@
+# variáveis que apenas comportam True ou False
+True 
+False
+if True:
+    print("Bloco if")
+else:
+    print("Bloco else nao será executado")
+
+# Operadores lógicos: and e or
+
+if True and True: 
+    print("Execução do bloco")
+
+if True and False: 
+    print("Bloco não será executado")
+
+if False and False:
+    print("Bloco não será executado")
+
+if True or False: 
+    print("Bloco será executado")
+
+if True or True:
+    print("Bloco será executado")
