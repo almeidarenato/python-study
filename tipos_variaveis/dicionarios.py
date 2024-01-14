83bd6c9efeeadce2d024c98cf2c07ad5d9b88871
Renato Almeida <renatoam@ymail.com>
2024-01-14 19:05:49 -0300

    adicionado métodos principais para uso com dicionarios

--- tipos_variaveis/dicionarios.py
+++ tipos_variaveis/dicionarios.py
@@ -11,3 +11,27 @@ print(f"{pessoa["cidade"]}")
 #Atribuir novos valores
 pessoa["sobrenome"] = "Almeida"
 print(f"{pessoa["sobrenome"]}")
+
+#Atualizar um valor existente
+pessoa["idade"] = 31
+print(f"{pessoa["idade"]}")
+
+#remover um par de chave e valor
+del pessoa["sobrenome"]
+print(f"{pessoa}")
+
+#Métodos : keys() , values() - lista dos valores , items() - lista de tuplas de pares de chave e valor
+chaves = pessoa.keys()
+print("Chaves do dicionario",chaves)
+#para que o elemento retornado se torne uma lista é possível fazer a conversão (ou cast) dessa forma
+chaves = list(pessoa.keys())
+print("Chaves do dicionario",chaves)
+valores = pessoa.values()
+print("Valores do dicionario",valores)
+
+items = pessoa.items()
+print("Items do dicionario",items)
+#para acessar o par desta tupla é necessário fazer da seguinte forma:
+items = list(pessoa.items())
+print(f"Primeira chave e valor: {items[0][0]} = {items[0][1]}")
+
