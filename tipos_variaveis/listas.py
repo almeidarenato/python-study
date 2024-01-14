43d607f545835527bb3bda7881b85bfb1e83ddfc
Renato Almeida <renatoam@ymail.com>
2024-01-14 18:08:40 -0300

    incluído o tipo de variável lista e metodos para manipulação

---
+++ tipos_variaveis/listas.py
@@ -0,0 +1,17 @@
+# Lista - coleção de elementos ordenáveis e imutáveis
+
+# Declaração
+minha_lista = [1,2,3,4,5,"texto",True,False]
+
+# cada elemento possui um indice
+print(f"Minha lista de exemplo {minha_lista}")
+#ou
+print(f"minha_lista[0]: {minha_lista[0]}")
+print(f"minha_lista[5]: {minha_lista[5]}")
+
+# fatiar uma lista , pegar uma parte dela ou slice
+print(f"minha_lista[1:6]: {minha_lista[1:6]}")
+# também é possível pegar do começo em diante
+print(f"minha_lista[:5]: {minha_lista[:5]}")
+# do elemento até o fim
+print(f"minha_lista[3:]: {minha_lista[3:]}")
