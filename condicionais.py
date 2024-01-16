03bcb5ac6fa3c2c1ad0541b7b38e52c44d2bd357
Renato Almeida <renatoam@ymail.com>
2024-01-15 21:07:14 -0300

    adicionando condicionais

---
+++ condicionais.py
@@ -0,0 +1,26 @@
+# if , elif e else
+
+# exemplo if/ else - o espaço após o if é importante pois indica que o comando está dentro do if
+idade = 17
+
+if idade >= 18:
+ print("Você é maior de idade")
+elif idade >= 12:
+ print("Você é um adolescente")
+else:
+ print("Você é menor de idade")
+
+ if idade == 19:
+  print("Você tem 19 anos")
+
+if idade < 18:
+ print("Você é menor de idade")
+
+if idade != 10: 
+  print ("Você não tem 10 anos")
+
+# if else em uma linha
+  
+  mensagem = "Pode tirar carteira de habilitação" if idade >= 18 else "Você não pode tirar a carteira de habilitação"
+
+  print(mensagem)
