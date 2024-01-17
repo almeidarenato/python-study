925baf48b799972cdb97797209071fc11e555259
Renato Almeida <renatoam@ymail.com>
2024-01-16 22:52:50 -0300

    adicionado funcao range  len e enumerate

--- loops.py
+++ loops.py
@@ -27,3 +27,27 @@ print("\nFor Dicionario ")
 
 for chave,valor in pessoa.items():
     print(f"{chave}:{valor}")
+
+# otimização 
+    
+    #range(): intervalo numérico 
+    # [0,1,2,3,4,5,6,7,8,9]
+print(list(range(0,10)))
+
+for numero in range(5):
+    print("numero",numero)
+
+# utilizando a função range() com len() para percorrer listas
+lista = [1,2,3,4,5]
+for indice in range(0,len(lista)):
+    print(lista[indice])
+    if(lista[indice] ==3 ):
+     lista[indice] =0
+print(lista)
+
+#enumerate() retorna a lista com o indice e o valor
+lista_enumerate = ["a","b","c"]
+for indice,elemento in enumerate(lista_enumerate):
+    print(f"{indice},{elemento}")
+
+print(enumerate(lista_enumerate))
