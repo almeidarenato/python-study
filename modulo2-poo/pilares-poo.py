85ac918bf6db4ba1be159299c6ac147a594e4c01
Renato Almeida <renatoam@ymail.com>
2024-01-22 23:16:52 -0300

    adicionando um exemplo de encapsulamento

--- modulo2-poo/pilares-poo.py
+++ modulo2-poo/pilares-poo.py
@@ -27,3 +27,38 @@ class Gato(Animal):
     def emitir_som(self):
         return "Miau!"
     
+dog = Cachorro(nome="Rex")
+cat = Gato(nome="Felix")
+print(dog.emitir_som())
+print(cat.emitir_som())
+animais = [dog,cat]
+
+for animal in animais:
+    print(f"O {animal.nome} emite o som: {animal.emitir_som()}")
+
+# Exemplo de encapsulamento
+# eu garanto que estou manipulando o valor desta variável de uma maneira correta
+    
+class ContaBancaria:
+    def __init__(self,saldo) -> None:
+        self.__saldo = saldo # Atributo privado definido como "__" antes do nome. Ninguem terá acesso fora da classe
+    
+    def depositar(self,valor):
+        if valor >0:
+            self.__saldo += valor
+    
+    def sacar(self,valor):
+        if valor > 0 and valor <= self.__saldo:
+            self.__saldo -= valor
+    
+    def consultar_saldo(self):
+        return self.__saldo
+    
+conta = ContaBancaria(saldo=1000)
+print(f"saldo da conta bancária {conta.consultar_saldo()}")
+conta.depositar(valor=500)
+print(f"saldo da conta bancária {conta.consultar_saldo()}")
+conta.depositar(valor=-500) ## não será permitido
+print(f"saldo da conta bancária {conta.consultar_saldo()}")
+conta.sacar(500)
+print(f"saldo da conta bancária {conta.consultar_saldo()}")
