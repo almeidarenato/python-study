c7940e6a42c25e84bdafdb44b3bc590be81ee34c
Renato Almeida <renatoam@ymail.com>
2024-01-28 23:36:05 -0300

    feat: instruções do que é o Flask e como instalar as dependencias via requirements

---
+++ modulo3-flask/flask.py
@@ -0,0 +1,11 @@
+# Flask
+# Framework Python para construção de APIs
+# Conhecido pela sua simplicidade e facilidade . 
+# Escalável e Flexível 
+# Um dos Principais frameworks do Python
+# Pinterest, LinkedIn , Netflix e Uber utilizam o Flask
+# Para instalar pip3 install Flask 
+# Para instalar uma versão específica podemos usar pip3 install Flask==2.3.0 (exemplo)
+# O arquivo requirements.txt no python serve para listar as dependencias
+# Para instalar os modulos a partir do arquivo de Dependências basta usar 
+# pip3 install -r requirements.txt
