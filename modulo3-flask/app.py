069d6499b0a4259809015edef81a666e823c7a13
Renato Almeida <renatoam@ymail.com>
2024-01-30 00:05:49 -0300

    chore : adicionado detalhamento sobre API rest

--- modulo3-flask/app.py
+++ modulo3-flask/app.py
@@ -9,6 +9,15 @@
 # O arquivo requirements.txt no python serve para listar as dependencias
 # Para instalar os modulos a partir do arquivo de Dependências basta usar 
 # pip3 install -r requirements.txt
+
+
+# Métodos GET , POST , PUT , DELETE , PATCH
+# Rest (Representational State Transfer) Obrigatoriamente usamos JSON e XML para representar
+# Api Rest x Api Restful
+# Api Rest = estilo de arquitetura para desenvolvimento de API
+# Rest princípios/regras que vão permear as comunicações entre os sistemas
+# Quando a API respeita todos os padrões de Rest , são Restful
+
 from flask import Flask
 
 app = Flask(__name__)
